70dc6e899bf0b1f2e480941f0184ac7992c924a1
darden40 <darden40@yahoo.com>
2017-12-13 19:02:57 -0600

    this does not matter

---
+++ HelloCodergirl/HelloCodergirl/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloCodergirl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name ="Meschele";
+            Console.WriteLine($"Good Morning {name}!");
+            Console.ReadLine();
+        }
+    }
+}
